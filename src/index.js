8ab160f2a8cec06693929e8c58e737693f11b332
刘鹏波 <liupengbo1@100tal.com>
2019-08-30 16:05:19 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+"use strict"
+
+import _ from 'lodash';
+import printMe from './print';
+
+function component(){
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+
+  element.innerHTML = _.join(["hello",'webpack','']);
+  btn .innerHTML = 'click me and check the console';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
